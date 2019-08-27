d66f6a939e64b9d191977fe4178678b477dc8bee
OlgaZ <olga.z.kordik@gmail.com>
2019-08-27 16:31:23 +0300

    Renamed class MainActivity members

--- app/src/main/java/com/study/noteskotlin/MainActivity.kt
+++ app/src/main/java/com/study/noteskotlin/MainActivity.kt
@@ -2,7 +2,6 @@ package com.study.noteskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.study.noteskotlin.adapters.RecyclerAdapter
@@ -11,10 +10,10 @@ import com.study.noteskotlin.models.Note
 class MainActivity : AppCompatActivity() {
 
 //    private val TAG = "NotesListActivity"
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-    private val notes: ArrayList<Note> = ArrayList()
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mViewAdapter: RecyclerView.Adapter<*>
+    private lateinit var mViewManager: RecyclerView.LayoutManager
+    private val mNotes: ArrayList<Note> = ArrayList()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,23 +22,23 @@ class MainActivity : AppCompatActivity() {
 
         addNotes()
 
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = RecyclerAdapter(notes)
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
-            layoutManager = viewManager
-            adapter = viewAdapter
+        mViewManager = LinearLayoutManager(this)
+        mViewAdapter = RecyclerAdapter(mNotes)
+        mRecyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            layoutManager = mViewManager
+            adapter = mViewAdapter
         }
 
 //        Log.d(TAG, "onCreate: $note2")
     }
 
     private fun addNotes() {
-        notes.add(Note("title 1", "content 1", "00:01"))
-        notes.add(Note("title 2", "content 2", "00:02"))
-        notes.add(Note("title 3", "content 3", "00:03"))
-        notes.add(Note("title 4", "content 4", "00:04"))
-        notes.add(Note("title 5", "content 5", "00:05"))
-        notes.add(Note("title 6", "content 6", "00:06"))
-        notes.add(Note("title 7", "content 7", "00:07"))
+        mNotes.add(Note("title 1", "content 1", "00:01"))
+        mNotes.add(Note("title 2", "content 2", "00:02"))
+        mNotes.add(Note("title 3", "content 3", "00:03"))
+        mNotes.add(Note("title 4", "content 4", "00:04"))
+        mNotes.add(Note("title 5", "content 5", "00:05"))
+        mNotes.add(Note("title 6", "content 6", "00:06"))
+        mNotes.add(Note("title 7", "content 7", "00:07"))
     }
 }
