7292e94258501688bebbaf694ff28176283da110
OlgaZ <olga.z.kordik@gmail.com>
2019-09-22 15:04:32 +0300

    Setting Initial Note Properties

--- app/src/main/java/com/study/noteskotlin/NoteActivity.kt
+++ app/src/main/java/com/study/noteskotlin/NoteActivity.kt
@@ -2,16 +2,59 @@ package com.study.noteskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
 import com.study.noteskotlin.models.Note
+import kotlinx.android.synthetic.main.activity_note.*
+import kotlinx.android.synthetic.main.note_toolbar.*
 
 class NoteActivity : AppCompatActivity() {
+
+    // UI components
+    private lateinit var mLinedEditText: LinedEditText
+    private lateinit var mEditTitle: EditText
+    private lateinit var mViewTitle: TextView
+
+    // vars
+    private var mIsNewNote = false
+    private lateinit var mNoteInitial: Note
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
 
-        val note = intent.getParcelableExtra<Note>("selected_note")
-        Toast.makeText(this, "note ${note.title}", Toast.LENGTH_SHORT).show()
+        mLinedEditText = edit_text_note
+        mEditTitle = noteEditTitle
+        mViewTitle = noteTextTitle
+
+        if (getIncomingIntent()) {
+            // this is a new note (EDIT MODE)
+            setNewNoteProperties()
+        } else {
+            // this is note a new note (VIEW MODE)
+            setNoteProperties()
+        }
     }
+
+    private fun getIncomingIntent() : Boolean {
+        if (intent.hasExtra("selected_note")) {
+            mNoteInitial = intent.getParcelableExtra<Note>("selected_note")
+            mIsNewNote = false
+            return false
+        }
+        mIsNewNote = true
+        return true
+    }
+
+    private fun setNewNoteProperties() {
+        mViewTitle.text = "Note title"
+        mEditTitle.setText("Note title")
+    }
+
+    private fun setNoteProperties() {
+        mViewTitle.text = mNoteInitial.title
+        mEditTitle.setText(mNoteInitial.title)
+        mLinedEditText.setText(mNoteInitial.content)
+    }
+
 }
